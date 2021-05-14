1c9e54e33e2741cb5d17d0dc07fc0fed91bfa650
shyamrupadas <s.mksimov@gmail.com>
2021-05-14 15:08:20 +0300

    Base markup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" className="logo-img" alt="Logo"/>
       </header>
+      <nav className='nav'>
+        <div>Profile</div>
+        <div>Messages</div>
+        <div>News</div>
+        <div>Music</div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300" alt="Image"/>
+        </div>
+        <div>
+          ava + description +
+        </div>
+        <div>
+          my post
+          <div>
+            new post
+          </div>
+
+        </div>
+        <div>
+          <div>Post1</div>
+          <div>Post2</div>
+        </div>
+        </div>
     </div>
   );
 }
