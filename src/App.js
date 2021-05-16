178a739043a22f8eacae54bd27b36ed7faa4b888
shyamrupadas <s.mksimov@gmail.com>
2021-05-16 17:26:11 +0300

    Routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import './App.css';
 import Header from "./modules/Header/Header";
-import Nav from "./modules/Nav/Nav";
+import Navbar from "./modules/Nav/Navbar";
 import Profile from "./modules/Profile/Profile";
+import {BrowserRouter, Route} from "react-router-dom";
 import Dialogs from "./modules/Dialogs/Dialogs";
+import News from "./modules/News/News";
+import Music from "./modules/Music/Music";
+import Settings from "./modules/Settings/Settings";
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header/>
-      <Nav/>
-      <div className='app-wrapper-content'>
-        <Dialogs/>
-        {/*<Profile />*/}
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header/>
+        <Navbar/>
+        <div className='app-wrapper-content'>
+          <Route path='/profile' component={Profile}/>
+          <Route path='/dialogs' component={Dialogs}/>
+          <Route path='/news' component={News}/>
+          <Route path='/music' component={Music}/>
+          <Route path='/settings' component={Settings}/>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
