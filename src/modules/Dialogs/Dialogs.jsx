bb116e8f5c6ba0fbc765da00a1465ccf5b2be526
shyamrupadas <s.mksimov@gmail.com>
2021-05-16 18:52:01 +0300

    tmp

--- src/modules/Dialogs/Dialogs.jsx
+++ src/modules/Dialogs/Dialogs.jsx
@@ -2,8 +2,20 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>Shyam Rupa</div>
+        <div className={s.dialog}>Vasliy</div>
+        <div className={s.dialog}>Uncle Gena</div>
+        <div className={s.dialog}>Valdemar</div>
+        <div className={s.dialog}>Tatyana</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hello</div>
+        <div className={s.message}>What's up?</div>
+        <div className={s.message}>Merhaba</div>
+        <div className={s.message}>Namaste!</div>
+      </div>
     </div>
   )
 }
