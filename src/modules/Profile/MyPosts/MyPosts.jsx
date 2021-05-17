09fed7a8d27fd7d4ef69d36859f1c11ee2680902
shyamrupadas <s.mksimov@gmail.com>
2021-05-17 20:07:44 +0300

    refactoring Profile

--- src/modules/Profile/MyPosts/MyPosts.jsx
+++ src/modules/Profile/MyPosts/MyPosts.jsx
@@ -3,10 +3,15 @@ import Post from "./Post/Post";
 
 const MyPosts = () => {
   return (
-    <div>
-      My post
+    <div className={s.postsBlock}>
+      <h3>My posts</h3>
       <div>
-        new post
+        <div>
+          <textarea>Что у вас нового?</textarea>
+        </div>
+        <div>
+          <button>Add post</button>
+        </div>
       </div>
       <div className={s.posts}>
         <Post message='Привет, мир!' like='5'/>
