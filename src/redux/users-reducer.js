63066dc8aa4841d88c2c78a8cef38daad94e9255
shyamrupadas <s.mksimov@gmail.com>
2021-05-29 21:14:37 +0300

    add BLL for Users

---
+++ src/redux/users-reducer.js
@@ -0,0 +1,71 @@
+const FOLLOW = 'FOLLOW';
+const UNFOLLOW = 'UNFOLLOW';
+const SET_USERS = 'SET-USERS';
+
+const initialState = {
+  users: [
+    /*{
+      id: 1,
+      followed: false,
+      fullName: 'Vasiliy Ivanovich',
+      avatarSrc: 'https://hypeava.ru/uploads/posts/2018-05/1527186681_1.jpg',
+      status: 'I\'m a man',
+      location: {country: 'Russia', city: 'Moscow'}
+    },
+    {
+      id: 2,
+      followed: true,
+      fullName: 'Petyka',
+      avatarSrc: 'https://hypeava.ru/uploads/posts/2018-05/1527186681_1.jpg',
+      status: 'I\'m a little man',
+      location: {country: 'Russia', city: 'Uryupinsk'}
+    },
+    {
+      id: 3,
+      followed: false,
+      fullName: 'Bill Gates',
+      avatarSrc: 'https://hypeava.ru/uploads/posts/2018-05/1527186681_1.jpg',
+      status: 'I\'m a boss',
+      location: {country: 'USU', city: 'New-York'}
+    },*/
+  ]
+
+};
+
+const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FOLLOW:
+      return {
+        ...state,
+        users: state.users.map(u => {
+          if (u.id === action.userId) {
+            return {...u, followed: true};
+          }
+          return u;
+        })
+      }
+
+    case UNFOLLOW:
+      return {
+        ...state,
+        users: state.users.map(u => {
+          if (u.id === action.userId) {
+            return {...u, followed: false};
+          }
+          return u;
+        })
+      }
+
+    case SET_USERS:
+      return {...state, users: [...state.users, ...action.users]}
+
+    default:
+      return state;
+  }
+}
+
+export const followAC = (userId) => ({type: FOLLOW, userId})
+export const unfollowAC = (userId) => ({type: UNFOLLOW, userId})
+export const setUsersAC = (users) => ({type: SET_USERS, users})
+
+export default usersReducer;
