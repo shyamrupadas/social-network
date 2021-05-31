bc1cd3d6d37c43b26dd7544bc900ca622648d1d9
shyamrupadas <s.mksimov@gmail.com>
2021-05-31 21:21:44 +0300

    add setUsers to constructor of Users

--- src/modules/Users/Users.jsx
+++ src/modules/Users/Users.jsx
@@ -5,29 +5,32 @@ import userPhoto from "../../assets/images/user.jpg";
 
 class Users extends React.Component {
 
-  getUsers = () => {
-    if (this.props.users.length === 0) {
-      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+  constructor(props) {
+    super(props);
 
-        this.props.setUsers(response.data.items);
-      });
-    }
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+      this.props.setUsers(response.data.items);
+    });
   }
 
   render() {
     return (
-      <div>
-        <button onClick={this.getUsers}>Get users</button>
+      <div className={styles.wrapper}>
         {
           this.props.users.map(u => <div key={u.id}>
           <span>
             <div>
-              <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} alt="Some img"/>
+              <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}
+                   alt="Some img"/>
             </div>
             <div>
               {u.followed
-                ? <button onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button>
-                : <button onClick={() => {this.props.follow(u.id)}}>Follow</button>}
+                ? <button onClick={() => {
+                  this.props.unfollow(u.id)
+                }}>Unfollow</button>
+                : <button onClick={() => {
+                  this.props.follow(u.id)
+                }}>Follow</button>}
 
             </div>
           </span>
