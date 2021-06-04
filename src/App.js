6adc00a903f665a6aa97d84257894e7549ce2ad3
shyamrupadas <s.mksimov@gmail.com>
2021-06-04 23:37:03 +0300

    refactor preloader as component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import Header from "./modules/Header/Header";
-import Profile from "./modules/Profile/Profile";
+import Header from "./components/Header/Header";
+import Profile from "./components/Profile/Profile";
 import {Route} from "react-router-dom";
-import News from "./modules/News/News";
-import Music from "./modules/Music/Music";
-import Settings from "./modules/Settings/Settings";
-import Sidebar from "./modules/Sidebar/Sidebar";
-import DialogsContainer from "./modules/Dialogs/DialogsContainer";
-import UsersContainer from "./modules/Users/UsersContainer";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import Sidebar from "./components/Sidebar/Sidebar";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
 
 const App = (props) => {
 
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -9,7 +9,7 @@ import {
 } from '../../redux/users-reducer';
 import * as axios from 'axios';
 import Users from './Users';
-import preloader from '../../assets/images/preloader.gif';
+import Preloader from "../common/Preloader/Preloader";
 
 
 class UsersContainer extends React.Component {
@@ -37,7 +37,7 @@ class UsersContainer extends React.Component {
 
   render() {
     return <>
-      {this.props.isFetching ? <img src={preloader}/> : null}
+      {this.props.isFetching ? <Preloader/> : null}
       <Users totalUsersCount={this.props.totalUsersCount}
              pageSize={this.props.pageSize}
              currentPage={this.props.currentPage}
