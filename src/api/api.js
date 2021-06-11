3529f10b54dc83c7c9f8f8e148b3857811127f54
shyamrupadas <s.mksimov@gmail.com>
2021-06-11 07:17:05 +0300

    added axios.create to api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,43 +1,36 @@
 import * as axios from "axios";
 
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  headers: {
+    'API-KEY': '91cc1011-293a-4a6f-955d-371d85cc4175'
+  }
+});
+
 export const getUsers = (currentPage = 1, pageSize = 10) => {
-  return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-      {
-        withCredentials: true,
-      })
+  return instance.get(`users?page=${currentPage}&count=${pageSize}`)
       .then(response => {
         return response.data;
       });
 }
 
 export const unfollowUser = (id) => {
-  return axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {
-    withCredentials: true,
-    headers: {
-      'API-KEY': '91cc1011-293a-4a6f-955d-371d85cc4175'
-    }
-  })
+  return instance.delete(`follow/${id}`)
     .then(response => {
     return response.data;
   });
 }
 
 export const followUser = (id) => {
-  return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {}, {
-    withCredentials: true,
-    headers: {
-      'API-KEY': '91cc1011-293a-4a6f-955d-371d85cc4175'
-    }
-  })
+  return instance.post(`follow/${id}`)
     .then(response => {
       return response.data;
     });
 }
 
 export const isAuthorised = () => {
-  return axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
-    withCredentials: true
-  })
+  return axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`)
     .then(response => {
       return response.data;
     });
