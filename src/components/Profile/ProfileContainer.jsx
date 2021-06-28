945a7e9358b147bc2cba5e04ef9e6882b949c68c
shyamrupadas <s.mksimov@gmail.com>
2021-06-28 19:40:59 +0300

    added protect to Profile

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -11,8 +11,10 @@ class ProfileContainer extends React.Component {
 
     let userId = this.props.match.params.userId;
     if (!userId) {
-
       userId = this.props.authorizerUserId;
+      if (!userId) {
+       this.props.history.push('/login');
+      }
     }
     this.props.getUserProfile(userId);
     this.props.getStatus(userId);
