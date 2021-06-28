eb345a80c7887b26deece3cca8f16611e21e8b7c
shyamrupadas <s.mksimov@gmail.com>
2021-06-28 19:43:57 +0300

    delete protect to Users

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -6,7 +6,6 @@ import {
 } from '../../redux/users-reducer';
 import Users from './Users';
 import Preloader from "../common/Preloader/Preloader";
-import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 import {compose} from "redux";
 
 
@@ -47,7 +46,6 @@ let mapStateToProps = (state) => {
 }
 
 export default compose(
-  withAuthRedirect,
   connect(mapStateToProps,
     {follow, unfollow, setUsers,
       setCurrentPage, setTotalUsersCount, toggleIsFetching, getUsers})
