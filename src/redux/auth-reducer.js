181f4777dd0db221bddc3c3d3908769f29254e2a
shyamrupadas <s.mksimov@gmail.com>
2021-07-02 14:17:17 +0300

    refactored auth-reducer to async-await

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -29,36 +29,32 @@ export const setAuthUserData = (id, login, email, isAuth) => ({
     {id, login, email, isAuth}
 });
 
-export const checkAuthorised = () => (dispatch) => {
- return authAPI.me()
-    .then(response => {
-      if (response.data.resultCode === 0) {
-        let {id, login, email} = response.data.data;
-        dispatch(setAuthUserData(id, login, email, true));
-      }
-    });
+export const checkAuthorised = () => async (dispatch) => {
+  let response = await authAPI.me();
+
+  if (response.data.resultCode === 0) {
+    let {id, login, email} = response.data.data;
+    dispatch(setAuthUserData(id, login, email, true));
+  }
 }
 
-export const login = (email, password, rememberMe) => (dispatch) => {
+export const login = (email, password, rememberMe) => async (dispatch) => {
+  let response = await authAPI.login(email, password, rememberMe);
 
-  authAPI.login(email, password, rememberMe)
-    .then(response => {
-      if (response.data.resultCode === 0) {
-        dispatch(checkAuthorised());
-      } else {
-        let message = response.data.messages.length > 0 ? response.data.messages : 'Some error';
-        dispatch(stopSubmit('login', {_error: message}));
-      }
-    });
+  if (response.data.resultCode === 0) {
+    dispatch(checkAuthorised());
+  } else {
+    let message = response.data.messages.length > 0 ? response.data.messages : 'Some error';
+    dispatch(stopSubmit('login', {_error: message}));
+  }
 }
 
-export const logout = () => (dispatch) => {
-  authAPI.logout()
-    .then(response => {
-      if (response.data.resultCode === 0) {
-        dispatch(setAuthUserData(null, null, null, false));
-      }
-    });
+export const logout = () => async (dispatch) => {
+  let response = await authAPI.logout();
+
+  if (response.data.resultCode === 0) {
+    dispatch(setAuthUserData(null, null, null, false));
+  }
 }
 
 export default authReducer;
