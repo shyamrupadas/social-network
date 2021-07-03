ce86982f1cdbf0668deebe9477903dd787a56bdf
shyamrupadas <s.mksimov@gmail.com>
2021-07-03 09:54:25 +0300

    extracted User from Users

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,44 +2,39 @@ import s from "./ProfileInfo.module.css";
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
-const ProfileInfo = (props) => {
+const ProfileInfo = ({profile, status, updateStatus}) => {
 
-  if (!props.profile) {
+  if (!profile) {
     return <Preloader/>
   }
 
   return (
     <div>
-      {/*<div>
-        <img
-          src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'
-          alt="Picture"/>
-      </div>*/}
       <div className={s.descriptionBlock}>
-        <img src={props.profile.photos.large}/>
-        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
+        <img src={profile.photos.large}/>
+        <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
         <div>
-          <h3>{props.profile.fullName}</h3>
+          <h3>{profile.fullName}</h3>
         </div>
         <div>
-          <p>Обо мне: {props.profile.aboutMe}</p>
+          <p>Обо мне: {profile.aboutMe}</p>
         </div>
         <div>
-          <p>Ищу работу: {props.profile.lookingForAJob === true ? 'Да' : 'Нет'}</p>
+          <p>Ищу работу: {profile.lookingForAJob === true ? 'Да' : 'Нет'}</p>
         </div>
           <div>
-            <p>Подробнее: {props.profile.lookingForAJobDescription && props.profile.lookingForAJobDescription}</p>
+            <p>Подробнее: {profile.lookingForAJobDescription && profile.lookingForAJobDescription}</p>
         </div>
         <div>
           <h3>Контакты</h3>
-          {props.profile.contacts.facebook && <p>facebook: {props.profile.contacts.facebook}</p>}
-          {props.profile.contacts.github && <p>github: {props.profile.contacts.github}</p>}
-          {props.profile.contacts.instagram && <p>instagram: {props.profile.contacts.instagram}</p>}
-          {props.profile.contacts.mainLink && <p>mainLink: {props.profile.contacts.mainLink}</p>}
-          {props.profile.contacts.twitter && <p>twitter: {props.profile.contacts.twitter}</p>}
-          {props.profile.contacts.vk && <p>vk: {props.profile.contacts.vk}</p>}
-          {props.profile.contacts.website && <p>website: {props.profile.contacts.website}</p>}
-          {props.profile.contacts.youtube && <p>youtube: {props.profile.contacts.youtube}</p>}
+          {profile.contacts.facebook && <p>facebook: {profile.contacts.facebook}</p>}
+          {profile.contacts.github && <p>github: {profile.contacts.github}</p>}
+          {profile.contacts.instagram && <p>instagram: {profile.contacts.instagram}</p>}
+          {profile.contacts.mainLink && <p>mainLink: {profile.contacts.mainLink}</p>}
+          {profile.contacts.twitter && <p>twitter: {profile.contacts.twitter}</p>}
+          {profile.contacts.vk && <p>vk: {profile.contacts.vk}</p>}
+          {profile.contacts.website && <p>website: {profile.contacts.website}</p>}
+          {profile.contacts.youtube && <p>youtube: {profile.contacts.youtube}</p>}
 
         </div>
       </div>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,51 +1,23 @@
 import styles from "./Users.module.css";
-import userPhoto from "../../assets/images/user.jpg";
 import React from "react";
 import Paginator from "../common/Paginator/Paginator";
-import {NavLink} from "react-router-dom";
+import User from "./User";
 
 let Users = (props) => {
 
   return <div className={styles.wrapper}>
-    <Paginator onPageChanged={props.onPageChanged} totalUsersCount={props.totalUsersCount}
-               pageSize={props.pageSize} currentPage={props.currentPage}/>
+    <Paginator onPageChanged={props.onPageChanged}
+               totalUsersCount={props.totalUsersCount}
+               pageSize={props.pageSize}
+               currentPage={props.currentPage}/>
     <div>
     {
-      props.users.map(u => <div key={u.id}>
-          <span>
-            <div>
-              <NavLink to={'/profile/' + u.id}>
-              <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}
-                   alt="Some img"/>
-                </NavLink>
-            </div>
-            <div>
-              {u.followed
-                ? <button disabled={props.followingInProgress
-                  .some(id => id === u.id)}
-                          onClick={() => { props.unfollow(u.id); }}>
-                  Unfollow</button>
-                : <button disabled={props.followingInProgress
-                  .some(id => id === u.id)} onClick={() => { props.follow(u.id); }}>
-                  Follow</button>}
-            </div>
-          </span>
-        <span>
-            <span>
-              <div>
-                {u.name}
-              </div>
-              <div>
-                {u.status}
-              </div>
-            </span>
-            <span>
-              <div>{"u.location.country"}</div>
-              <div>{"u.location.city"}</div>
-            </span>
-          </span>
-
-      </div>)
+      props.users.map(u => <User key={u.id}
+                                 user={u}
+                                 followingInProgress={props.followingInProgress}
+                                 unfollow={props.unfollow}
+                                 follow={props.follow} />
+      )
     }
     </div>
     <Paginator onPageChanged={props.onPageChanged}
