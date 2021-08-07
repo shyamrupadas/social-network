f52e1e837ec807bb94b999c31fc7c8e887ce29b6
Sergei Maksimov <s.mksimov@gmail.com>
2021-08-07 22:30:55 +0300

    added types to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,22 @@
 import s from './Header.module.css';
 import logo from '../../assets/images/logo.png';
-import {NavLink} from "react-router-dom";
+import { NavLink } from 'react-router-dom';
+import React from 'react';
 
-const Header = (props) => {
+type PropsType = {
+  isAuth: boolean
+  login: string
+  logout: () => void
+};
+
+const Header: React.FC<PropsType> = (props) => {
   return (
     <header className={s.header}>
       <NavLink to={'/'}><img src={logo} alt='Logo'/></NavLink>
       <div className={s.loginBlock}>
-        { props.isAuth
+        {props.isAuth
           ? <div>{props.login} - <button onClick={props.logout}>Log out</button></div>
-          : <NavLink to={'/login'}>Login</NavLink> }
+          : <NavLink to={'/login'}>Login</NavLink>}
       </div>
     </header>
   )
