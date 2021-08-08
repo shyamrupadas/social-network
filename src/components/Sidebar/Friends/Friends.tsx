cd0d76d7af61a73a99e15493fcb4c3dec599406c
Sergei Maksimov <s.mksimov@gmail.com>
2021-08-08 23:38:28 +0300

    added types to Friends

--- src/components/Sidebar/Friends/Friends.tsx
+++ src/components/Sidebar/Friends/Friends.tsx
@@ -1,16 +1,17 @@
 import s from './Friends.module.css';
-import Friend from "./Friend/Friend";
+import Friend from './Friend/Friend';
+import React from 'react';
 
-const Friends = () => {
+const Friends: React.FC = () => {
   return (
     <div className={s.wrapper}>
       <div className={s.header}>
         Friends:
       </div>
       <div className={s.content}>
-        <Friend/>
-        <Friend/>
-        <Friend/>
+        <Friend />
+        <Friend />
+        <Friend />
       </div>
     </div>
   )
