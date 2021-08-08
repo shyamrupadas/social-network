d6b1f0e4a10d0352dfb57bfe62e02814014fe346
Sergei Maksimov <s.mksimov@gmail.com>
2021-08-08 23:19:12 +0300

    added types to Preloader

--- src/components/common/Preloader/Preloader.js
+++
@@ -1,8 +0,0 @@
-import preloader from "../../../assets/images/preloader.gif";
-import React from "react";
-
-const Preloader = () => {
-  return <img src={preloader} alt='preloader'/>
-}
-
-export default Preloader;
---
+++ src/components/common/Preloader/Preloader.tsx
@@ -0,0 +1,8 @@
+import preloader from '../../../assets/images/preloader.gif';
+import React from 'react';
+
+const Preloader: React.FC = () => {
+  return <img src={preloader} alt='preloader'/>
+}
+
+export default Preloader;
