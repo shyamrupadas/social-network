52ab54d3dcc7b7face34b02afe47ff1eb9888319
Sergei Maksimov.com <s.mksimov@gmail.com>
2021-09-02 00:16:45 +0300

    added antd layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import './App.css';
 import 'antd/dist/antd.css';
-import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import { BrowserRouter, NavLink, Redirect, Route } from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
-import Sidebar from './components/Sidebar/Sidebar';
 import { UsersPage } from './components/Users/UsersPage';
-import HeaderContainer from './components/Header/HeaderContainer';
 import { LoginPage } from './components/Login/LoginPage';
 import React, { Component, Suspense } from 'react';
 import { connect, Provider } from 'react-redux';
 import { initializeApp } from './redux/app-reducer';
 import Preloader from './components/common/Preloader/Preloader';
 import store, { AppStateType } from './redux/redux-store';
+import { Layout, Menu } from 'antd';
+import s from './components/Sidebar/Nav/Navbar.module.css';
+
+const { Header, Content, Footer, Sider } = Layout;
 
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
@@ -43,22 +45,61 @@ class App extends Component<MapPropsType & DispatchPropsType> {
     }
 
     return (
-      <div className='app-wrapper'>
-        <HeaderContainer />
-        <Sidebar />
-        <div className='app-wrapper-content'>
-          <Suspense fallback={<Preloader />}>
-            <Route path='/profile/:userId?' render={() => <ProfileContainer />} />
-            <Route path='/dialogs' render={() => <DialogsContainer />} />
-          </Suspense>
-          <Route exact path='/' render={() => <Redirect to={'/profile'} />} />
-          <Route path='/users' render={() => <UsersPage pageTitle={'My social network'} />} />
-          <Route path='/news' render={() => <News />} />
-          <Route path='/music' render={() => <Music />} />
-          <Route path='/settings' render={() => <Settings />} />
-          <Route path='/login' render={() => <LoginPage />} />
-        </div>
-      </div>
+      <Layout>
+        <Header className="header">
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+            <Sider className="site-layout-background" width={200}>
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                style={{ height: '100%' }}
+              >
+                <Menu.Item key="1"><NavLink to='/profile' activeClassName={s.active}>Profile</NavLink></Menu.Item>
+                <Menu.Item key="2"><NavLink to='/dialogs' activeClassName={s.active}>Messages</NavLink></Menu.Item>
+                <Menu.Item key="3"><NavLink to='/users' activeClassName={s.active}>Users</NavLink></Menu.Item>
+              </Menu>
+            </Sider>
+            <Content style={{ padding: '0 24px', minHeight: 280 }}>
+              <Suspense fallback={<Preloader />}>
+                <Route path='/profile/:userId?' render={() => <ProfileContainer />} />
+                <Route path='/dialogs' render={() => <DialogsContainer />} />
+              </Suspense>
+              <Route exact path='/' render={() => <Redirect to={'/profile'} />} />
+              <Route path='/users' render={() => <UsersPage pageTitle={'My social network'} />} />
+              <Route path='/news' render={() => <News />} />
+              <Route path='/music' render={() => <Music />} />
+              <Route path='/settings' render={() => <Settings />} />
+              <Route path='/login' render={() => <LoginPage />} />
+            </Content>
+          </Layout>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+      // <div className='app-wrapper'>
+      //   <HeaderContainer />
+      //   <Sidebar />
+      //   <div className='app-wrapper-content'>
+      //     <Suspense fallback={<Preloader />}>
+      //       <Route path='/profile/:userId?' render={() => <ProfileContainer />} />
+      //       <Route path='/dialogs' render={() => <DialogsContainer />} />
+      //     </Suspense>
+      //     <Route exact path='/' render={() => <Redirect to={'/profile'} />} />
+      //     <Route path='/users' render={() => <UsersPage pageTitle={'My social network'} />} />
+      //     <Route path='/news' render={() => <News />} />
+      //     <Route path='/music' render={() => <Music />} />
+      //     <Route path='/settings' render={() => <Settings />} />
+      //     <Route path='/login' render={() => <LoginPage />} />
+      //   </div>
+      // </div>
     );
   }
 }
